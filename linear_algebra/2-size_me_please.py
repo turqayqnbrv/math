08cfd4676ca95aa270076fd0e1a97bdf4cc43217
turqayqnbrv <128743545+turqayqnbrv@users.noreply.github.com>
2026-05-02 22:24:05 +0400

    Enhance matrix_shape function with docstring and type check
    Added docstring to describe the function's purpose and improved type checking.

--- linear_algebra/2-size_me_please.py
+++ linear_algebra/2-size_me_please.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
+"""Calculates the shape of a matrix"""
+
+
 def matrix_shape(matrix):
-    if type(matrix[0]) != list:
+    """Returns the shape of a matrix as a list of integers"""
+    if not isinstance(matrix[0], list):
         return [len(matrix)]
     else:
         return [len(matrix)] + matrix_shape(matrix[0])
