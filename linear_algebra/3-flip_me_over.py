20633bf43fdd34492f354f3e189bb7a46555c83f
turqayqnbrv <128743545+turqayqnbrv@users.noreply.github.com>
2026-05-02 22:23:08 +0400

    Add docstring to matrix_transpose function
    Added docstring to matrix_transpose function for clarity.

--- linear_algebra/3-flip_me_over.py
+++ linear_algebra/3-flip_me_over.py
@@ -1,3 +1,15 @@
 #!/usr/bin/env python3
+"""Transpose function to check a new matrix transposed"""
+
+
 def matrix_transpose(matrix):
+    """
+    Returns a new matrix transposed
+
+    Args:
+        matrix: given list of lists
+
+    Returns:
+        new_matrix: Transposed matrix
+    """
     return [[row[i] for row in matrix] for i in range(len(matrix[0]))]
