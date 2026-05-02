e432b5b187fbe4ed96c2fdbefabed0016799dc93
turqayqnbrv <128743545+turqayqnbrv@users.noreply.github.com>
2026-05-02 20:52:07 +0400

    Delete 2-size_me_please.py

--- 2-size_me_please.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-def matrix_shape(matrix):
-    shape = []
-    current = matrix
-    while isinstance(current, list):
-        shape.append(len(current))
-        current = current[0]
-    return shape
