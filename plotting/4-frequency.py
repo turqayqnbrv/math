fd1fce6b073b78680db9d97b3687bbf6ee44a439
turqayqnbrv <128743545+turqayqnbrv@users.noreply.github.com>
2026-05-31 16:02:33 +0400

    Add frequency plotting function for student grades

---
+++ plotting/4-frequency.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+'''Histogram usage'''
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def frequency():
+    '''The function plots a frequency of the grades'''
+    np.random.seed(5)
+    student_grades = np.random.normal(68, 15, 50)
+    plt.figure(figsize=(6.4, 4.8))
+
+    bin_edges = np.arange(0, 110, 10)
+    plt.hist(student_grades, bins=bin_edges, edgecolor='black')
+    
+    plt.xlabel("Grades")
+    plt.ylabel("Number of Students")
+    plt.title("Project A")
+    
+    plt.xticks(bin_edges)
+    plt.xlim(0, 100)
+    plt.show()
