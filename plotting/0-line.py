68f65009bd3bf81444a9b694c234842bcabac348
turqayqnbrv <128743545+turqayqnbrv@users.noreply.github.com>
2026-05-31 15:18:20 +0400

    Fix x range in line plot function
    Changed the x range in the plot from 0 to 10 to 0 to 11.

--- plotting/0-line.py
+++ plotting/0-line.py
@@ -7,9 +7,6 @@ def line():
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
 
-    x = np.arange(0, 11)
-    plt.xlim(0, 10)
-    plt.plot(x, y, color='red', label='y = x^3')
+    x = np.arange(0, 10)
+    plt.plot(x, y, color='red')
     plt.show()
-
-line()
