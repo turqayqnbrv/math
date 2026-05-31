d08c8aba2228179cce8f192ca15f38e9e95e3335
turqayqnbrv <128743545+turqayqnbrv@users.noreply.github.com>
2026-05-31 15:21:08 +0400

    Update 0-line.py

--- plotting/0-line.py
+++ plotting/0-line.py
@@ -3,10 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def line():
-    
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
-
     x = np.arange(0, 11)
     plt.plot(x, y, color='red')
     plt.xlim(0, 10)
