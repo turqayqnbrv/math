17044914b348f1b472e5e0e1bc9ec169994d0179
turqayqnbrv <128743545+turqayqnbrv@users.noreply.github.com>
2026-05-31 15:23:06 +0400

    Enhance documentation in 0-line.py
    Added module docstring and function docstring for clarity.

--- plotting/0-line.py
+++ plotting/0-line.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
+"""This module provides a function to plot a cubic line graph."""
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 def line():
+    """Plots a red cubic line graph with the x-axis ranging from 0 to 10."""
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
     x = np.arange(0, 11)
