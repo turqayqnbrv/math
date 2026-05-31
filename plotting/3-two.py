74c4d0254ccea0117d7d850136a8a04c1692cf93
turqayqnbrv <128743545+turqayqnbrv@users.noreply.github.com>
2026-05-31 15:50:26 +0400

    Add script for plotting C-14 and Ra-226 decay
    This script plots the exponential decay of C-14 and Ra-226 over time, displaying both on a single figure.

---
+++ plotting/3-two.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+'''Two plot holding on one figure'''
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def two():
+    '''Plotting C-14 and Ra-226'''
+    x = np.arange(0, 21000, 1000)
+    r = np.log(0.5)
+    t1 = 5730
+    t2 = 1600
+    y1 = np.exp((r / t1) * x)
+    y2 = np.exp((r / t2) * x)
+    plt.figure(figsize=(6.4, 4.8))
+
+    plt.xlabel("Time (years)")
+    plt.ylabel("Fraction Remaining")
+    plt.title("Exponential Decay of Radioactive Elements")
+    plt.xlim(0, 20000)
+    plt.ylim(0, 1)
+    plt.plot(x, y1, 'r--')
+    plt.plot(x, y2, color='g')
+    plt.legend()
+    plt.show()
