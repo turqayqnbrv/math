34904c715463d259f6ae15be3e2f714eb9b5f4bd
turqayqnbrv <128743545+turqayqnbrv@users.noreply.github.com>
2026-05-31 15:15:33 +0400

    Add files via upload

---
+++ plotting/0-line.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+def line():
+    
+    y = np.arange(0, 11) ** 3
+    plt.figure(figsize=(6.4, 4.8))
+
+    x = np.arange(0, 11)
+    plt.xlim(0, 10)
+    plt.plot(x, y, color='red', label='y = x^3')
+    plt.show()
+
+line()
