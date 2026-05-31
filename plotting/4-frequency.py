cdcfa1ef496d008dce25ee68e7bc2b7f8c621848
turqayqnbrv <128743545+turqayqnbrv@users.noreply.github.com>
2026-05-31 16:09:53 +0400

    Adjust histogram bin edges for student grades

--- plotting/4-frequency.py
+++ plotting/4-frequency.py
@@ -10,7 +10,7 @@ def frequency():
     student_grades = np.random.normal(68, 15, 50)
     plt.figure(figsize=(6.4, 4.8))
 
-    bin_edges = np.arange(0, 110, 10)
+    bin_edges = np.arange(0, 101, 10)
     plt.hist(student_grades, bins=bin_edges, edgecolor='black')
     
     plt.xlabel("Grades")
@@ -18,4 +18,5 @@ def frequency():
     plt.title("Project A")
     plt.xlim(0, 100)
     plt.ylim(0, 30)
+    plt.xticks(bin_edges)
     plt.show()
